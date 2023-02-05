4d01d83eec4d0f477f8ecfc1013b75a1ed4527f8
Trust <r.tolpeznikas@gmail.com>
2023-02-05 11:25:08 +0100

    Playin around

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -40,16 +31,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _OutText = "bussin";
+  int _StateCounter = -1;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _StateCounter++;
+      if(_StateCounter > 8) _StateCounter = 0;
+
+      switch(_StateCounter) {
+        case 0: {
+          _OutText = "I've been";
+        }
+        break;
+        case 1: {
+          _OutText = "in the";
+        }
+        break;
+        case 2: {
+          _OutText = "kitchen";
+        }
+        break;
+        case 3: {
+          _OutText = "saucin";
+        }
+        break;
+        case 4: {
+          _OutText = "saucin";
+        }
+        break;
+        case 5: {
+          _OutText = "I've been";
+        }
+        break;
+        case 6: {
+          _OutText = "Winnin";
+        }
+        break;
+        case 7: {
+          _OutText = "Bossin";
+        }
+        break;
+        case 8: {
+          _OutText = "Bossin";
+        }
+        break;
+      }
     });
   }
 
@@ -91,10 +118,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Current State of flex',
             ),
             Text(
-              '$_counter',
+              _OutText,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -103,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.ad_units),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
